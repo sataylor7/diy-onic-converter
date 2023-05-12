e3a1168ae544b3f7bfe47d8e49e103291aa7067f
SATaylor <engelstern7@yahoo.com>
2023-05-12 13:28:18 -0500

    checking the existance of the p tag and then adding some error handling

--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -10,10 +10,15 @@ const diyOnicConverter = (textContentContainerSelector) => {
    * The function should then convert the content of every p element within the elements matched by that selector so that every word is formatted in a style that is similar to bionic reading.
    */
   // check for existence of p in this case in the container *ideally this shouldnt be hard coded
-  // will return a node list will need to convert to an array
-  const tags = container.querySelectorAll('p');
+  // will return a node list will need to convert to an array => this may have actually been updated so that nodelist has forEach
+  const tags = [].slice.call(container.querySelectorAll('p'));
   // if the length is greater than 0 then perform the actions
-  console.log(tags.length);
+  if (tags.length > 0) {
+    console.log('continue working ');
+  } else {
+    console.log('sorry there are no valid tags to convert');
+    return;
+  }
 };
 
 // Allow global access so that this can be executed from the console.
